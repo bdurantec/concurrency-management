ac9e93e3609cbdbf7dc691d3d81226e315e49b1d
bdurantec <brunodurantec@gmail.com>
2024-02-12 11:12:32 -0300

    Removing @JsonProperty and organizing POM.xml

--- src/main/java/br/com/bdurantec/rinhadebackend/concurrencymanagement/domain/model/Account.java
+++ src/main/java/br/com/bdurantec/rinhadebackend/concurrencymanagement/domain/model/Account.java
@@ -1,11 +1,7 @@
 package br.com.bdurantec.rinhadebackend.concurrencymanagement.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record Account(
-		@JsonProperty("limite")
 		Long limit,
-		@JsonProperty("saldo")
 		Long balance
 ) {
 }
--- src/main/java/br/com/bdurantec/rinhadebackend/concurrencymanagement/domain/model/TransactionResult.java
+++ src/main/java/br/com/bdurantec/rinhadebackend/concurrencymanagement/domain/model/TransactionResult.java
@@ -1,11 +1,7 @@
 package br.com.bdurantec.rinhadebackend.concurrencymanagement.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record TransactionResult(
-		@JsonProperty("limite")
 		Long limit,
-		@JsonProperty("saldo")
 		Long balance
 ) {
 }
