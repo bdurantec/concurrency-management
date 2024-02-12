217537f89621dec402f4272b03ccea7bcd4b0a2b
bdurantec <brunodurantec@gmail.com>
2024-02-12 11:52:11 -0300

    Adding H2 dependency to Local-Tests (PostgreSQL will be configured before deploy)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries (id, limite, saldo_inicial) VALUES (1, 1000, 50);
+INSERT INTO countries (id, limite, saldo_inicial) VALUES (2, 1000, 50);
+INSERT INTO countries (id, limite, saldo_inicial) VALUES (3, 1000, 50);
+INSERT INTO countries (id, limite, saldo_inicial) VALUES (4, 1000, 50);
+INSERT INTO countries (id, limite, saldo_inicial) VALUES (5, 1000, 50);
--- target/generated-sources/annotations/br/com/bdurantec/rinhadebackend/concurrencymanagement/application/mapper/TransactionMapperImpl.java
+++ target/generated-sources/annotations/br/com/bdurantec/rinhadebackend/concurrencymanagement/application/mapper/TransactionMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-12T10:44:30-0300",
+    date = "2024-02-12T11:50:15-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (Oracle Corporation)"
 )
 public class TransactionMapperImpl implements TransactionMapper {
